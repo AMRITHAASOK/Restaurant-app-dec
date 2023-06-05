d4e052e7382208e30c1eb753bacfa744aa219d3f
amrithaasok <amrithaasok.aa@gamil.com>
2023-06-05 15:47:01 +0530

    Restaurant app

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+function Header() {
+  return (
+    <div>
+          <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">
+          <i class="fa-solid fa-utensils fa-bounce"></i>
+            {' '}
+            Restro Cafe
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    
+    </div>
+  )
+}
+
+export default Header
